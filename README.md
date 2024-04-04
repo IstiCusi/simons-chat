302245dca6022b7ec4751174df96da8c4f5730bd
Stephan Strauss <StephanStrauss@gmx.ch>
2024-04-04 09:47:01 +0200

    First commit of this very simple chat client

---
+++ README.md
@@ -0,0 +1,40 @@
+# Simon's Chat clinet - A Very Simple and unsafe Chat Client
+
+## Overview
+
+This project showcases a simple chat application designed to illustrate core concepts of network programming, concurrency, and design patterns within a minimalistic framework. It serves as an educational tool for those looking to understand the basics of creating networked applications with C and Python.
+Additionally, this intentionally simplistic chat client will serve as a testing ground for exploring various exploit techniques in the future, enhancing understanding of security vulnerabilities within networked applications.
+
+### Server
+
+The server is inspired by "Network Programming with C" by Van Winkel, highlighting the essentials of socket programming in C. It's a straightforward implementation aimed at demonstrating how to handle client connections, data transmission, and concurrency through multi-threading, laying the foundation for more complex networked applications with
+no transmission checks on completness.
+
+### Client
+
+On the client side, the application is developed in Python, making use of `raylibpy` for the graphical user interface. This part of the project illustrates how Python applications can integrate with libraries to handle real-time graphical updates, user input, and network communication effectively. The application dynamically manages its dependencies, ensuring a smooth setup and execution process for the user.
+
+## Key Concepts
+
+- **Network Programming**: Utilizes sockets for communication between the chat server and client, adhering to TCP for reliable data transmission.
+- **Concurrency**: Employs threading on both the server and client sides to manage multiple connections and perform tasks like sending, receiving, and rendering messages concurrently.
+- **Observer/Observable Pattern**: Implemented in the Python client to update the chat interface in response to new messages, demonstrating decoupled communication between application components.
+- **Dynamic Dependency Management**: The Python client automatically installs missing packages, highlighting a user-friendly approach to dependency resolution.
+
+## Installation and Usage
+
+To run the server, compile `server.c` using a C compiler (e.g., `gcc`) and execute the resulting binary. The client can be run with Python after ensuring `raylibpy` and any other dependencies are installed. Note that the application automatically installs Python dependencies upon startup.
+
+### Font
+
+FiraCode is used in the chat interface, chosen for its readability and support for programming ligatures, enhancing the visual presentation of the chat.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Acknowledgments
+
+- Server inspired by "Network Programming with C" by Van Winkels.
+- FiraCode font used for enhancing the chat interface's readability.
+
---
+++ chat.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+
+import subprocess
+import pkg_resources
+import sys
+
+required = {'raylib-py'} 
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    print("The following packes will be installed now", missing)
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+
+import raylibpy
+import time
+import socket
+import threading
+from typing import Optional
+from enum import Enum
+
+class MsgType(Enum):
+    ME = 1
+    FRIEND = 2
+
+
+class ChatNetworkHandler:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket: Optional[socket.socket] = None
+        self.observers = []
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        threading.Thread(target=self.receive_message_loop, daemon=True).start()
+
+    def send_message(self, message):
+        assert self.socket is not None, "Socket not initialized"
+        self.socket.sendall(message.encode())
+
+    def receive_message_loop(self):
+        assert self.socket is not None, "Socket not initialized"
+        try:
+            while True:
+                message = self.socket.recv(4096).decode()
+                if message:
+                    self.notify_observers(message)
+                else:
+                    break
+        finally:
+            self.socket.close()
+
+    def register_observer(self, observer):
+        if observer not in self.observers:
+            self.observers.append(observer)
+
+    def notify_observers(self, message):
+        for observer in self.observers:
+            observer.update(message)
+
+    def close(self):
+        assert self.socket is not None, "Socket not initialized"
+        self.socket.close()
+
+
+class ChatClient:
+    def __init__(self, font="./Font.ttf", network_address="localhost", port=1602):
+
+        self.screen_width = 1210
+        self.screen_height = 450
+
+        raylibpy.init_window(self.screen_width, self.screen_height, "Simon's chat")
+        raylibpy.set_target_fps(60)
+
+        self.font = raylibpy.load_font(font)
+        raylibpy.set_texture_filter(self.font.texture, raylibpy.TEXTURE_FILTER_TRILINEAR)
+        self.font_size = self.font.base_size
+        self.char_width = raylibpy.measure_text_ex(self.font, "A", self.font_size, 1)[0]
+        self.max_length = (int)(self.screen_width / self.char_width) - 7
+
+        self.network_address = network_address
+        self.port = port
+
+        self.network_handler = ChatNetworkHandler(network_address, port)
+        self.network_handler.register_observer(self)
+
+        self.messages = []
+
+        self.input_text = ""
+        self.cursor_position = 0
+        self.cursor_visible = True
+
+        self.last_blink_time = time.time()
+        self.blink_interval = 0.5
+
+        self.backspace_pressed = False
+        self.backspace_timer = 0
+        self.backspace_delay = 0.07
+        self.last_backspace_time = time.time()
+
+    def handle_input(self):
+        max_length = self.max_length 
+        key = raylibpy.get_char_pressed()
+        while key > 0:
+            if key >= 32 and key <= 125:
+                if len(self.input_text) < max_length or self.cursor_position < max_length:
+                    self.input_text = self.input_text[:self.cursor_position] + chr(key) + self.input_text[self.cursor_position:]
+                    self.cursor_position += 1
+            key = raylibpy.get_char_pressed()
+
+    def handle_cursor_movement(self):
+        if raylibpy.is_key_pressed(raylibpy.KEY_RIGHT):
+            if self.cursor_position < len(self.input_text):
+                self.cursor_position += 1
+        elif raylibpy.is_key_pressed(raylibpy.KEY_LEFT):
+            if self.cursor_position > 0:
+                self.cursor_position -= 1
+
+    def handle_backspace(self):
+        current_time = time.time()
+        if raylibpy.is_key_down(raylibpy.KEY_BACKSPACE):
+            if not self.backspace_pressed or current_time - self.last_backspace_time > self.backspace_delay:
+                if self.cursor_position > 0:
+                    self.input_text = self.input_text[:self.cursor_position - 1] + self.input_text[self.cursor_position:]
+                    self.cursor_position -= 1
+                    self.last_backspace_time = current_time
+                self.backspace_pressed = True
+        else:
+            self.backspace_pressed = False
+
+    def handle_enter(self):
+        if raylibpy.is_key_pressed(raylibpy.KEY_ENTER) and self.input_text.strip() != "":
+            self.send_message(self.input_text)
+            self.input_text = ""
+            self.cursor_position = 0
+
+    def update_cursor_blink(self):
+        if time.time() - self.last_blink_time > self.blink_interval:
+            self.cursor_visible = not self.cursor_visible
+            self.last_blink_time = time.time()
+
+    def draw(self):
+        raylibpy.begin_drawing()
+
+        raylibpy.clear_background(raylibpy.RAYWHITE)
+
+        for i, message in enumerate(self.messages[-12:]):
+            text_color = raylibpy.BLACK if message[1] == MsgType.ME else raylibpy.GRAY
+            raylibpy.draw_text_ex(self.font, message[0], raylibpy.Vector2(20, 10 + i * 30), self.font_size, 1, text_color)
+
+        raylibpy.draw_rectangle(10, self.screen_height - 50, self.screen_width - 20, 40, raylibpy.DARKBLUE)
+        raylibpy.draw_text_ex(self.font, self.input_text, raylibpy.Vector2(20, self.screen_height - 45), self.font_size, 1, raylibpy.YELLOW)
+
+        if self.cursor_visible:
+            cursor_x = 20 + raylibpy.measure_text_ex(self.font, self.input_text[:self.cursor_position], self.font_size, 1).x
+            raylibpy.draw_rectangle(int(cursor_x), self.screen_height - 45, self.char_width, 30, raylibpy.PINK)
+            if self.cursor_position < len(self.input_text):
+                letter_under_cursor = self.input_text[self.cursor_position]
+                raylibpy.draw_text_ex(self.font, letter_under_cursor, raylibpy.Vector2(int(cursor_x), self.screen_height - 45), self.font_size, 1, raylibpy.WHITE)
+
+        raylibpy.end_drawing()
+
+    def update(self, message):
+        self.messages.append([ message, MsgType.FRIEND ])
+
+    def connect_to_server(self):
+        self.network_handler.connect()
+
+    def send_message(self, message):
+        self.network_handler.send_message(message)
+        self.messages.append([ message, MsgType.ME ])
+
+    def run(self):
+        while not raylibpy.window_should_close():
+            self.handle_input()
+            self.handle_backspace()
+            self.handle_cursor_movement()
+            self.handle_enter()
+            self.update_cursor_blink()
+            self.draw()
+        raylibpy.close_window()
+
+if __name__ == "__main__":
+    client = ChatClient()
+    client.connect_to_server()
+    client.run()
+
